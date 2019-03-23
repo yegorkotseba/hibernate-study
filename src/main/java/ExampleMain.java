a97d8d788681fe7e726669fc470aeeb5a783fc80
yegorkotseba <yegor.kotseba@gmail.com>
2019-03-23 17:58:48 +0200

    change db credentials

--- src/main/java/ExampleMain.java
+++ src/main/java/ExampleMain.java
@@ -15,7 +15,7 @@ public class ExampleMain {
     public static void main(String[] args) throws Exception{
 
         Class.forName ("org.h2.Driver");
-        Connection conn = DriverManager.getConnection ("jdbc:h2:~/test", "sa","");
+        Connection conn = DriverManager.getConnection ("jdbc:h2:~/test", "","");
         Statement stmt = conn.createStatement();
         stmt.executeUpdate("DROP TABLE AGENTS");
         stmt.executeUpdate("DROP TABLE CUSTOMER");
